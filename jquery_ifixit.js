2d5836f4edcda942f35986f48ea3f8b342c2fc31
Chris Opperwall <copperwa@calpoly.edu>
2014-01-26 20:08:31 -0800

    Removes droppable helper text on device drop
    The droppable area displays a helper text that states "Drop Devices Here", which now now detached when a device is dropped into the area.
    
    The reason it is being detached, rather than deleted, is because once the ability to remove devices from "Your Devices" is implemented, the helper text will be re-appended when the area is empty again.

--- jquery_ifixit.js
+++ jquery_ifixit.js
@@ -1,4 +1,9 @@
 $(function() {
+   /* Used to hold the "Drop Devices Here" element after dropping a device
+    * into the droppable area.
+    */
+   var detached_helper_text;
+
     $( ".device" ).draggable({ revert: "invalid", stack: ".device" });
  
     $( "#drop_device" ).droppable({
@@ -6,6 +11,11 @@ $(function() {
       hoverClass: "ui-state-hover",
       drop: function( event, ui ) {
         $(this).append(ui.draggable.css( { position: "static"}));
+
+        if ($.contains(document.getElementById("drop_device"),
+           document.getElementById("drop_inner"))) {
+            detached_helper_text = $("#drop_inner").detach();
+        }
       }
     });
   });
